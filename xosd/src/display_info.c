ba996f842585ece8afa7f0ba6c326b5f153e6274
Schlauf <schlauf@Kubuntu.myguest.virtualbox.org>
2022-11-10 17:48:35 -0800

    Completed the display_info tool

--- xosd/src/display_info.c
+++ xosd/src/display_info.c
@@ -13,8 +13,33 @@ printerror()
 int
 main(int argc, char *argv[])
 {
+  xosd *osd;
+  osd = xosd_create(2);
+
+  if (0 != xosd_set_outline_offset(osd, 1)) {
+    printerror();
+  }
+
+  if (0 != xosd_set_font(osd, (char *) osd_default_font)) {
+    printerror();
+  }
+
+  if (0 != xosd_set_timeout(osd, 2)) {
+    printerror();
+  }
   
   display_info();
+
+  if (0 != xosd_wait_until_no_display(osd)) {
+    printerror();
+  }
+
+  sleep(10);
   
+  if (0 != xosd_destroy(osd)) {
+    printerror();
+  }
+
+
   return EXIT_SUCCESS;
 }
